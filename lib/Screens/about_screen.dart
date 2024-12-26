940b80fd2043a7f5b62e033804f037a4cfd0f23d
Hussnain <hussnain.waheed864@techservicesstudio.com>
2024-12-27 02:23:18 +0500

    about screen complete

--- lib/Screens/about_screen.dart
+++ lib/Screens/about_screen.dart
@@ -1,13 +1,234 @@
 
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gtcci_app/Screens/execuitive_members_screen.dart';
+import 'package:flutter_gtcci_app/Screens/gtcci_screen.dart';
+import 'package:flutter_gtcci_app/Screens/office_bearer_screen.dart';
 
-class AboutScreen extends StatelessWidget{
+class AboutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+
+    return Scaffold(
+
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff2af598), Color(0xff009efd),
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 40),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  Image.asset("assets/images/gtcci2.png",
+                    width: 100,
+                    height: 100,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10, bottom: 51),
+                    child: Container(
+                      margin: EdgeInsets.only(top: 15),
+                      child: Text(
+                        "Gujrat Chamber Of Commerce \n and Industry",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'FontMain',
+                          fontStyle: FontStyle.italic,
+                          color: Colors.black.withOpacity(0.6),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  //Buttons
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color(0xff2af598), Color(0xff009efd),
+                                  ],
+                                ),
+                                borderRadius: BorderRadius.circular(12),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.white,
+                                    spreadRadius: 3,
+                                    blurRadius: 4,
+                                  ),
+                                ],
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // Handle button tap
+                                  Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => GtcciScreen() ),
+                                  );
+                                },
+                                child: Text(
+                                  'GTCCI',
+                                  style: TextStyle(
+                                      fontSize: 17, fontWeight: FontWeight.bold),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all<Color>(
+                                      Colors.green),
+                                  foregroundColor: MaterialStateProperty.all<Color>(
+                                      Colors.white),
+                                  padding:
+                                  MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.symmetric(
+                                        vertical: 30, horizontal: 75),
+                                  ),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                  textStyle: MaterialStateProperty.all<TextStyle>(
+                                    TextStyle(fontSize: 16),
+                                  ),
+                                  elevation: MaterialStateProperty.all<double>(5),
+                                ),
+                              ),
+                            ),
+
+                            SizedBox(height: 20),
+                            Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color(0xff2af598), Color(0xff009efd),
+                                  ],
+                                ),
+                                borderRadius: BorderRadius.circular(12),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.white, spreadRadius: 3, blurRadius: 4,
+                                  ),
+                                ],
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // Handle button tap
+                                  Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => ExecuitiveMemberScreen() ),
+                                  );
+                                },
+                                child: Text(
+                                  'EXECUITIVE MEMBERS',
+                                  style: TextStyle(
+                                      fontSize: 15, fontWeight: FontWeight.bold),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all<Color>(
+                                      Colors.green),
+                                  foregroundColor: MaterialStateProperty.all<Color>(
+                                      Colors.white),
+                                  padding:
+                                  MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.symmetric(
+                                        vertical: 32, horizontal: 20),
+                                  ),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                  textStyle: MaterialStateProperty.all<TextStyle>(
+                                    TextStyle(fontSize: 16),
+                                  ),
+                                  elevation: MaterialStateProperty.all<double>(5),
+                                ),
+                              ),
+                            ),
+
+                            SizedBox(height: 20),
+                            Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color(0xff2af598), Color(0xff009efd),
+                                  ],
+                                ),
+                                borderRadius: BorderRadius.circular(12),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.white, spreadRadius: 3, blurRadius: 4,
+                                  ),
+                                ],
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // Handle button tap
+                                  Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => OfficeBearerScreen() ),
+                                  );
+                                },
+                                child: Text(
+                                  'OFFICE BEARERS',
+                                  style: TextStyle(
+                                      fontSize: 15, fontWeight: FontWeight.bold),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all<Color>(
+                                      Colors.green),
+                                  foregroundColor: MaterialStateProperty.all<Color>(
+                                      Colors.white),
+                                  padding:
+                                  MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.symmetric(
+                                        vertical: 30, horizontal: 40),
+                                  ),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                  textStyle: MaterialStateProperty.all<TextStyle>(
+                                    TextStyle(fontSize: 16),
+                                  ),
+                                  elevation: MaterialStateProperty.all<double>(5),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+
+                ],
+              ),
+            )
+
+          ],
+        ),
+      ),
+
+
+    );
   }
 
 }
