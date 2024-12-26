99cd1884377060163a99d25cfe930014bb1d97d2
Hussnain <hussnain.waheed864@techservicesstudio.com>
2024-12-27 02:35:36 +0500

    Social Screen with functionalites of whatsapp

--- lib/Screens/social_screen.dart
+++ lib/Screens/social_screen.dart
@@ -1,13 +1,412 @@
-
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class SocialScreen extends StatelessWidget{
+class SocialScreen extends StatelessWidget {
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
 
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 60.2,
+        toolbarOpacity: 0.8,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(25),
+            bottomLeft: Radius.circular(25),
+          ),
+        ),
+        elevation: 0.00,
+        backgroundColor: Color(0xff2af598),
+        title: Text(
+          "Social",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        titleSpacing: 00.0,
+      ),
+
+
+
+      body: Padding(
+        padding: const EdgeInsets.all(17.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    "assets/images/gtcci2.png",
+                    width: 70,
+                    height: 70,
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            color: Color(0xff2af598),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(text: "  Gujrat Chamber "),
+                            TextSpan(
+                              text: "\n  of Commerce and Industry",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                height: 1.5,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: GridView(
+                        shrinkWrap: true,
+                        children: [
+
+                          //Whatsapp
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(25.0),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.red,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 5,
+                                            blurRadius: 7,
+                                            offset: Offset(0, 3), // changes position of shadow
+                                          ),
+                                        ],
+                                      ),
+                                      child:GestureDetector(
+                                        onTap: () {
+                                          openWhatsAppChooser(
+                                            "+923008622963", // Replace with your recipient's phone number
+                                            "Hello GTCCI! How Are You?", // Replace with your message
+                                          );
+                                        },
+
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(20),
+                                          child: Image.asset(
+                                            'assets/images/w.jpg',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                                // WhatsApp
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 6),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(1.0),
+                                    child: Text(
+                                      'WhatsApp',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        height: 0.2,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          //Facebook
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(25.0),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.red,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 5,
+                                            blurRadius: 7,
+                                            offset: Offset(0, 3), // changes position of shadow
+                                          ),
+                                        ],
+                                      ),
+                                      child:GestureDetector(
+                                        onTap: () {
+                                          launch("https://www.facebook.com/gujratchamberofcommerce/");
+                                        },
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(20),
+                                          child: Image.asset(
+                                            'assets/images/f.jpg',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                                // Facebook
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 6),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(1.0),
+                                    child: Text(
+                                      'Facebook',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        height: 0.2,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          //LinkedIn
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(25.0),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.red,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 5,
+                                            blurRadius: 7,
+                                            offset: Offset(0, 3), // changes position of shadow
+                                          ),
+                                        ],
+                                      ),
+                                      child:GestureDetector(
+                                        onTap: () {
+                                          launch('https://www.linkedin.com/company/gtcci/');
+
+                                        },
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(20),
+                                          child: Image.asset(
+                                            'assets/images/l.jpg',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                                // WhatsApp
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 6),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(1.0),
+                                    child: Text(
+                                      'LinkedIn',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        height: 0.2,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          //Twitter
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(25.0),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.red,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 5,
+                                            blurRadius: 7,
+                                            offset: Offset(0, 3), // changes position of shadow
+                                          ),
+                                        ],
+                                      ),
+                                      child:GestureDetector(
+                                        onTap: () {
+                                          launch("https://twitter.com/gujrat_chamber");
+                                        },
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(20),
+                                          child: Image.asset(
+                                            'assets/images/t.jpg',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                                // Twitter
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 6),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(1.0),
+                                    child: Text(
+                                      'Twitter',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        height: 0.2,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          //Location
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(25.0),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.red,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 5,
+                                            blurRadius: 7,
+                                            offset: Offset(0, 3), // changes position of shadow
+                                          ),
+                                        ],
+                                      ),
+                                      child:GestureDetector(
+                                        onTap: () {
+                                          launch('https://maps.app.goo.gl/r7s1EtNh6Eys7sRf8');
+
+                                        },
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(20),
+                                          child: Image.asset(
+                                            'assets/images/m.jpg',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                                // WhatsApp
+                                Container(
+                                  margin: EdgeInsets.only(left: 4),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(1.0),
+                                    child: Text(
+                                      'Location',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        height: 0.2,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 1,
+                          crossAxisSpacing: 10,
+                        )
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+void openWhatsAppChooser(String phoneNumber, String message) async {
+  String whatsappUrl = "https://wa.me/$phoneNumber/?text=${Uri.encodeFull(message)}";
+  String whatsappBusinessUrl = "https://api.whatsapp.com/send?phone=$phoneNumber&text=${Uri.encodeFull(message)}";
+
+  try {
+    await launch(whatsappUrl);
+  } catch (e) {
+    try {
+      await launch(whatsappBusinessUrl);
+    } catch (e) {
+      throw 'Could not launch either WhatsApp or WhatsApp Business';
+    }
+  }
 }
