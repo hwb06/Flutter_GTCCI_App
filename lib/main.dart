a7440c9db9ca44e5bf5531159a909852f3603a3c
Hussnain <hussnain.waheed864@techservicesstudio.com>
2024-12-27 03:01:42 +0500

    GTCCI App Completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'GTCCI App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: SplashScreen(),
